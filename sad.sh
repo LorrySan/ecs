025d296ef5414537dd7bbd386d232330c78129bf
LorrySan <shenluorui@gmail.com>
2025-11-09 11:47:47 +0800

    Create script to configure system settings and reboot
    This script adds a MOTD, reloads SSH, configures kernel parameters, applies them, and reboots the system.

---
+++ sad.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# 添加 MOTD 信息
+sudo tee -a /etc/motd <<'EOF'
+========================
+This image built by Sad Cloud.
+Build time: 2023-11-09 00:30:00 EST.
+========================
+EOF
+
+# 重载 SSH 服务
+sudo systemctl reload sshd
+
+# 配置系统内核参数
+sudo cat > /etc/sysctl.conf << EOF
+fs.file-max = 6815744
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.tcp_rmem=4096 87380 33554432
+net.ipv4.tcp_wmem=4096 16384 33554432
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+net.ipv6.conf.all.forwarding=1
+net.ipv6.conf.default.forwarding=1
+EOF
+
+# 应用系统内核参数
+sudo sysctl -p && sudo sysctl --system
+
+# 重启系统
+sudo reboot
